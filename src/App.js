3df617a76ff8b4a562801a6c0aa5f56cfd39b43b
surra7 <subum6302@gmail.com>
2024-02-15 14:40:48 +0900

    update chapter2. create 기능생성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
-export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      expenses: [
-        { id: 1, charge: '콜라', amount: 2000 },
-        { id: 2, charge: '빵', amount: 1000 },
-        { id: 3, charge: '맥북', amount: 20000 },
-      ]
-    }
-  }
+//export default class App extends Component {
+const App = () => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     expenses: [
+  //       { id: 1, charge: '콜라', amount: 2000 },
+  //       { id: 2, charge: '빵', amount: 1000 },
+  //       { id: 3, charge: '맥북', amount: 20000 },
+  //     ]
+  //   }
+  // }
 
   // 지출 데이터가 사용되는 곳은 App.js -> ExpenseList.js -> ExpenseItem.js
   // 데이터를 전달해 줘야함
@@ -24,37 +24,73 @@ export default class App extends Component {
   //   { id: 3, charge: '맥북', amount: 20000 },
   // ]
 
-  handleDelete = (id) => {
-    const newExpense = this.state.expenses.filter((expense) => expense.id !== id)
+  const [expenses, setExpenses] = useState([
+    { id: 1, charge: '콜라', amount: 2000 },
+    { id: 2, charge: '빵', amount: 1000 },
+    { id: 3, charge: '맥북', amount: 20000 },
+  ])
 
-    this.setState({ expenses: newExpense });
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
   }
 
-  render() {
-    return (
-      <main className="main-container">
-        <div className="sub-container">
-          <h1>장바구니</h1>
+  const handleAmount = (e) => {
+    setAmount(e.target.valueAsNumber);
+  }
 
-          <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-            {/* Expense Form */}
-            <ExpenseForm />
-          </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      // id는 랜덤 아이디, crypto 라이브러리 사용
+      const newExpense = { id: crypto.randomUUID(), charge, amount };
+      // 불변성을 지키기 위해 원래 배열을 복사하고 새로운 값을 넣어줌, expenses 바로 push하면 불변성을 지키지 못함
+      const newExpenses = [...expenses, newExpense];
+      setExpenses(newExpenses);
+      setCharge("");
+      setAmount(0);
+    } else {
+      console.error('error');
+    }
+  }
 
-          <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-            {/* Expense List */}
-            {/* this.initialExpenses 대신에 state를 이용해 값 전달 */}
-            <ExpenseList initialExpenses={this.state.expenses}
-              handleDelete={this.handleDelete} />
-          </div>
+  const handleDelete = (id) => {
+    // 함수형 컴포넌트에서는 this.state.expenses 대신 바로 state 이름인 expenses 사용
+    const newExpense = expenses.filter((expense) => expense.id !== id)
 
-          <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
-            <p style={{ fontSize: '2rem' }}>
-              총합계:
-            </p>
-          </div>
+    //this.setState({ expenses: newExpense });
+    // 함수형 컴포넌트에서는 this.setState 대신 setter인 setExpenses 사용
+    setExpenses(newExpense);
+  }
+
+  return (
+    <main className="main-container">
+      <div className="sub-container">
+        <h1>장바구니</h1>
+
+        <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+          {/* Expense Form */}
+          <ExpenseForm charge={charge} handleCharge={handleCharge}
+            amount={amount} handleAmount={handleAmount} handleSubmit={handleSubmit} />
         </div>
-      </main>
-    )
-  }
+
+        <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+          {/* Expense List */}
+          {/* this.initialExpenses 대신에 state를 이용해 값 전달 -> this.state.expenses*/}
+          <ExpenseList initialExpenses={expenses}
+            handleDelete={handleDelete} />
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
+          <p style={{ fontSize: '2rem' }}>
+            총합계:
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
+
+export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './ExpenseItem.css';
 import { MdDelete, MdEdit } from 'react-icons/md';
 
-export default class ExpenseItem extends Component {
-    render() {
-        return (
-            <li className='item'>
-                <div className='info'>
-                    <span className='expense'>{this.props.expense.charge}</span>
-                    <span className='amount'>{this.props.expense.amount}</span>
-                </div>
-                <div>
-                    <button className='edit-btn'>
-                        <MdEdit />
-                    </button>
-                    <button
-                        className='clear-btn'
-                        onClick={() => this.props.handleDelete(this.props.expense.id)}
-                    >
-                        <MdDelete />
-                    </button>
-                </div>
-            </li>
-        )
-    }
+//export default class ExpenseItem extends Component {
+const ExpenseItem = (props) => {
+    return (
+        <li className='item'>
+            <div className='info'>
+                <span className='expense'>{props.expense.charge}</span>
+                <span className='amount'>{props.expense.amount}</span>
+            </div>
+            <div>
+                <button className='edit-btn'>
+                    <MdEdit />
+                </button>
+                <button
+                    className='clear-btn'
+                    onClick={() => props.handleDelete(props.expense.id)}
+                >
+                    <MdDelete />
+                </button>
+            </div>
+        </li>
+    )
 }
+
+export default ExpenseItem;
