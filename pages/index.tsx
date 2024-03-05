993ab37bc7c2f18542301638843ca959070ea941
surra7 <subum6302@gmail.com>
2024-03-05 23:32:33 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import homeStyles from "@/styles/Home.module.css";
+import { GetStaticProps, NextPage } from "next";
+import { getSortedPostsData } from "@/lib/post";
+import Link from "next/link";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>Subum</title>
+      </Head>
+      <section className={homeStyles.headingMd}>
+        <p>[Subum Introduction]</p>
+        <p>(This is a website)</p>
+      </section>
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <span>{title}</span>
+              </Link>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
