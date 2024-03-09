4e74319847d56c091550b6603c0c9ffe6a32277f
surra7 <subum6302@gmail.com>
2024-03-09 22:20:17 +0900

    ~firebase 연결

--- App.js
+++ App.js
@@ -6,23 +6,24 @@ import { store } from "./redux/store";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./screens/LoginScreen";
-
-const Stack = createNativeStackNavigator();
+import app from "./firebase";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
             options={{ headerShown: false }}
-            name="Main"
-            component={MainScreen}
+            name="Login"
+            component={LoginScreen}
           />
           <Stack.Screen
             options={{ headerShown: false }}
-            name="Login"
-            component={LoginScreen}
+            name="Main"
+            component={MainScreen}
           />
         </Stack.Navigator>
       </NavigationContainer>
