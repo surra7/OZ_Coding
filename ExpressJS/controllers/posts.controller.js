a208100024f7f46f0e86440e5b4453e2381b96a8
surra7 <subum6302@gmail.com>
2024-03-25 13:31:29 +0900

    express 앱 완료

--- ExpressJS/controllers/posts.controller.js
+++ ExpressJS/controllers/posts.controller.js
@@ -2,7 +2,10 @@ const path = require("path");
 
 function getPost(req, res) {
   // res.send("<div><h1>Post Title</h1><p>This is a post</p></div>");
-  res.sendFile(path.join(__dirname, "..", "public", "images", "sky.jpg"));
+  // res.sendFile(path.join(__dirname, "..", "public", "images", "sky.jpg"));
+  res.render("posts", {
+    templateName: "post",
+  });
 }
 
 module.exports = {
--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -4,12 +4,18 @@ const usersRouter = require("./routes/users.router");
 const postsRouter = require("./routes/posts.router");
 
 const PORT = 4000;
-
+const path = require("path");
 const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+// public 경로 안에 있는 html, css, 이미지 파일등을 제공
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 // 그냥 req.body.name를 보내면 undefined가 나옴
 // -> 해결: express.json()사용 -> body를 파싱해줌
 app.use(express.json());
-
 // 미들웨어
 // next로 다음 미들웨어로 넘어가다가 마지막 메인 테스크를 마치면 지나갔던 미들웨어를 거꾸로 한번더 거쳐서 되돌아감
 app.use((req, res, next) => {
@@ -20,6 +26,12 @@ app.use((req, res, next) => {
   console.log(`end: ${req.method} ${req.baseUrl}${req.url} ${diffTime}ms`);
   // req.url은 라우터 인스턴스가 마운트된 URL 경로.
   // baseUrl을 넣어야 / users가 나옴. 아니면 그냥 / 만 나옴
+});
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    imageTitle: "It is a sky 2",
+  });
 });
 
 app.use("/users", usersRouter);
